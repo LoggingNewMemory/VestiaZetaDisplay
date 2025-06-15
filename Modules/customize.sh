7cd0d1a3046d351b87c66e754e375ae9f84ac375
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-06-15 19:03:12 +0700

    Modify customize.sh

--- Modules/customize.sh
+++ Modules/customize.sh
@@ -1,8 +1,8 @@
 LATESTARTSERVICE=true
 
-ui_print "🗡--------------------------------🗡"
-ui_print "  Vestia Zeta 120 Hz Refresh Rate   "
-ui_print "🗡--------------------------------🗡"
+ui_print "------------------------------------"
+ui_print "         Vestia Zeta Display        "
+ui_print "------------------------------------"
 ui_print "         By: Kanagawa Yamada        "
 ui_print "------------------------------------"
 ui_print " "
@@ -22,26 +22,27 @@ ui_print "RAM : $(free | grep Mem |  awk '{print $2}') "
 ui_print " "
 sleep 1.5
 
-ui_print "-----------------🗡-----------------"
+ui_print "------------------------------------"
 ui_print "            MODULE INFO             "
-ui_print "-----------------🗡-----------------"
+ui_print "------------------------------------"
 ui_print "Name : Vestia Zeta 120 Hz"
 ui_print "Version : 5.0"
 ui_print "Support Root : Magisk / KernelSU"
 ui_print " "
 sleep 1.5
 
-ui_print "    DO NOT INSTALL IF YOUR PHONE    "
-ui_print "     DOESN'T HAVE 120 Hz SCREEN     "
-ui_print " "
-
-sleep 3
 ui_print "       INSTALLING VESTIA ZETA       "
 ui_print " "
-sleep 1.5
 
 unzip -o "$ZIPFILE" 'VestiaZeta/*' -d $MODPATH >&2
 set_perm_recursive $MODPATH/VestiaZeta 0 0 0755 0644
 
-ui_print "     CAT INSTALLED SUCCESSFULLY     "
+sleep 3
+
+case "$((RANDOM % 4 + 1))" in
+1) ui_print "Aaa... Jangan dong" ;;
+2) ui_print "Kamu teh dibangunin dari tadi gak bangun bangun" ;;
+3) ui_print "Meow ~" ;;
+4) ui_print "I'M NOT A CAT!!" ;;
+esac
 sleep 1.5
