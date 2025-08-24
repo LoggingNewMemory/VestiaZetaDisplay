c19a9cb302c131eb29a34c3a9a7ebac19e1cac0e
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-08-24 19:36:21 +0700

    add banner

--- Modules/customize.sh
+++ Modules/customize.sh
@@ -26,7 +26,7 @@ ui_print "------------------------------------"
 ui_print "            MODULE INFO             "
 ui_print "------------------------------------"
 ui_print "Name : Vestia Zeta 120 Hz"
-ui_print "Version : 5.0"
+ui_print "Version : 6.0"
 ui_print "Support Root : Magisk / KernelSU"
 ui_print " "
 sleep 1.5
