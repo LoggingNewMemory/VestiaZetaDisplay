2ae2c0798ed70badc0400002a83667a4dc90c040
Kanagawa Yamada <yamadakernel.project@gmail.com>
2026-01-11 20:12:18 +0700

    Update to 6.0
    Signed-off-by: Kanagawa Yamada <yamadakernel.project@gmail.com>

--- Modules/customize.sh
+++ Modules/customize.sh
@@ -26,7 +26,7 @@ ui_print "------------------------------------"
 ui_print "            MODULE INFO             "
 ui_print "------------------------------------"
 ui_print "Name : Vestia Zeta Display"
-ui_print "Version : 6.0"
+ui_print "Version : 7.0"
 ui_print "Support Root : Magisk / KernelSU"
 ui_print " "
 sleep 1.5
