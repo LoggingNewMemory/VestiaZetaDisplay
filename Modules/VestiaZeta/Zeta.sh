a542275e640ec51a1ddd841a1a91383b555beee1
Kanagawa Yamada <yamadakernel.project@gmail.com>
2026-01-11 20:10:38 +0700

    Upstream from Raco
    Signed-off-by: Kanagawa Yamada <yamadakernel.project@gmail.com>

--- Modules/VestiaZeta/Zeta.sh
+++ Modules/VestiaZeta/Zeta.sh
@@ -1,14 +1,15 @@
-# Original Zeta Script
+max_rate=$(cmd display dump 2>/dev/null | grep -Eo 'fps=[0-9.]+' | cut -f2 -d= | sort -nr | head -n1 | cut -d . -f 1)
 
-service call SurfaceFlinger 1035 i32 0
+if [ -n "$max_rate" ] && [ "$max_rate" -gt 60 ]; then
+    # Apply settings to lock the refresh rate
+    settings put system min_refresh_rate "$max_rate"
+    settings put system peak_refresh_rate "$max_rate"
+    
+    # Apply persistent properties for some vendor implementations
+    setprop persist.sys.sf.refresh_rate "$max_rate"
+    setprop persist.vendor.display.refresh_rate "$max_rate"
+else
+    exit 1
+fi
 
-# Attempt to go universal
-# Get all available refresh rates
-rates=$(dumpsys display | tr ',' '\n' | awk -F= '/^ fps/ {v[$2]=1} END {for (r in v) print r}' | sort -nr)
-
-# Get highest rate and convert to integer
-max_rate=$(echo "$rates" | head -1 | cut -d. -f1)
-
-# Apply settings
-settings put system min_refresh_rate $max_rate
-settings put system peak_refresh_rate $max_rate
+exit 0
