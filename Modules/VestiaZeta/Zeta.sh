61a7e70aae2d603a6607034c4f91c898c3290c38
KanagawaYamada <yamadakernel.project@gmail.com>
2026-01-18 21:33:20 +0700

    Move all to system.prop
    Signed-off-by: KanagawaYamada <yamadakernel.project@gmail.com>

--- Modules/VestiaZeta/Zeta.sh
+++ Modules/VestiaZeta/Zeta.sh
@@ -9,18 +9,4 @@ else
     exit 1
 fi
 
-resetprop debug.graphics.game_default_frame_rate.disabled true
-resetprop debug.sf.disable_backpressure 1
-resetprop debug.sf.disable_hwc 1
-resetprop debug.sf.latch_unsignaled 1
-resetprop debug.sf.disable_client_composition_cache 1
-resetprop ro.surface_flinger.use_color_management false
-resetprop ro.surface_flinger.has_wide_color_display false
-resetprop ro.surface_flinger.has_hdr_display false
-resetprop persist.sys.sf.native_mode 1
-resetprop vendor.debug.mali.disable_afbc 1
-resetprop ro.vendor.ddk.set.afbc 0
-resetprop debug.gralloc.map_fb_memory 1
-resetprop debug.gralloc.enable_fb_ubwc 0
-
 exit 0
